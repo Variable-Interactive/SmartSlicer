f01526e50db98eea6d4d69db3c241d360887af7f
Variable <pkfayezakhtar@gmail.com>
2023-08-03 20:45:35 +0500

    Optimized the algorithm, it should now be roughly twice as fast

--- Example/Texture.gd
+++ Example/Texture.gd
@@ -2,10 +2,13 @@ extends TextureRect
 
 var rect_data: Dictionary
 
+# roughly 665886 with old imp
 func _on_Button_pressed() -> void:
+	var time = Time.get_ticks_usec()
 	var unpak := RegionUnpacker.new(10, 3)
 	rect_data = unpak.get_used_rects(texture.get_data())
 	$Control.show_preview(rect_data["rects"])
+	print(Time.get_ticks_usec() - time)
 
 
 
