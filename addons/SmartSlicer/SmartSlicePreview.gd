f633c10b60bf614fe9b09e49da85474108d70fff
Variable <77773850+Variable-ind@users.noreply.github.com>
2023-08-04 00:06:29 +0500

    More formatting

--- addons/SmartSlicer/SmartSlicePreview.gd
+++ addons/SmartSlicer/SmartSlicePreview.gd
@@ -1,11 +1,10 @@
 extends Control
 
 # add this as a child of the texturerect that contains the main spritesheet
+var color: Color = Color("6680ff")  # Set this to a theme color later
 var _sliced_rects: Array
 var _stretch_amount: float
 var _offset: Vector2
-
-var color: Color = Color("6680ff")  # Set this to a theme color later
 
 
 func show_preview(sliced_rects: Array) -> void:
@@ -29,7 +28,9 @@ func _draw() -> void:
 		draw_rect(scaled_rect, color, false)
 		# show number
 		draw_set_transform(_offset + scaled_rect.position, 0, Vector2.ONE)
-		var font: Font = Control.new().get_font("font")
+#		var font: Font = Control.new().get_font("font")
+		# replace with font used by pixelorama
+		var font: Font = Global.control.theme.default_font
 		var font_height := font.get_height()
 		draw_string(font, Vector2(1, font_height), str(i))
 		draw_set_transform(_offset, 0, Vector2.ONE)
