cce3e0a16353928b98e14fd16f12572431a71840
Variable <77773850+Variable-ind@users.noreply.github.com>
2023-08-10 22:50:40 +0500

    Allow AspectRatioContainer to have different ratios

--- addons/SmartSlicer/SmartSlicePreview.gd
+++ addons/SmartSlicer/SmartSlicePreview.gd
@@ -1,5 +1,6 @@
 extends Control
 ## Add this as a child of the texturerect that contains the main spritesheet
+## the stretch_mode of the texture muat be STRETCH_KEEP_ASPECT_CENTERED
 
 var color := Color("6680ff")  ## Set this to a theme color later
 var _sliced_rects: Array[Rect2i]
@@ -8,13 +9,13 @@ var _offset: Vector2
 
 
 func show_preview(sliced_rects: Array[Rect2i]) -> void:
-	var image: Image = get_parent().texture.get_image()
-	if image.get_size().x > image.get_size().y:
-		_stretch_amount = size.x / image.get_size().x
-	else:
-		_stretch_amount = size.y / image.get_size().y
+	var texture_rect = get_parent()
+	match texture_rect.stretch_mode:
+		texture_rect.STRETCH_KEEP_ASPECT_CENTERED:
+			stretch_keep_aspect_centered()
+		_:
+			return
 	_sliced_rects = sliced_rects.duplicate()
-	_offset = (0.5 * (size - (image.get_size() * _stretch_amount))).floor()
 	queue_redraw()
 
 
@@ -32,3 +33,22 @@ func _draw() -> void:
 		var font_height := font.get_height()
 		draw_string(font, Vector2(1, font_height), str(i))
 		draw_set_transform(_offset, 0, Vector2.ONE)
+
+
+# Setups for different stretch modes
+func stretch_keep_aspect_centered():
+	var texture_size = get_parent().texture.get_size()
+	var aspect = texture_size.x / texture_size.y
+	var occupied_image_size = texture_size
+	if texture_size.x < size.x and (size.x / aspect) <= size.y:
+		occupied_image_size.x = size.x
+		occupied_image_size.y = (size.x / aspect)
+	else:
+		occupied_image_size.y = size.y
+		occupied_image_size.x = (size.y * aspect)
+
+	if texture_size.x > texture_size.y:
+		_stretch_amount = occupied_image_size.x / texture_size.x
+	else:
+		_stretch_amount = occupied_image_size.y / texture_size.y
+	_offset = (0.5 * (size - (texture_size * _stretch_amount))).floor()
